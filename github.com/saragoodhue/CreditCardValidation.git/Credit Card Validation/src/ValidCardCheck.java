537214553aa3523ba914c8f1f2c9c6cc4bc337b5
sara.goodhue <sara.goodhue@MU-C1-12.mullen.pvt.k12.co.us>
2021-10-19 11:52:05 -0600

    does not work

---
+++ github.com/saragoodhue/CreditCardValidation.git/Credit Card Validation/src/ValidCardCheck.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+    
+public class ValidCardCheck
+	{
+		
+		static int numbers;
+
+		public static void main(String[] args) throws IOException
+			{
+			
+				
+				int counter = 0;
+				
+				while(numbers.hasNext())
+					{
+						for(int i = 0; i < 99; i ++)
+							{
+								
+							}
+					}
+			}
+		
+		public static void digitStrip() throws IOException
+		{
+			Scanner numbers = new Scanner(new File( "creditcardnumbers.txt" ));
+			int cardNumber = numbers.nextInt();
+
+			while (cardNumber > 0)
+				{
+					int digitCheck = cardNumber % 10;
+				}
+		}
+
+	}
