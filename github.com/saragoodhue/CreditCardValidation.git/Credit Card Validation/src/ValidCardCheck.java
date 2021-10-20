bf5c8a78f4e5ebf4209536c4940938f96548f492
sara.goodhue <sara.goodhue@MU-C1-12.mullen.pvt.k12.co.us>
2021-10-20 12:10:19 -0600

    digit strip and checking every other

--- github.com/saragoodhue/CreditCardValidation.git/Credit Card Validation/src/ValidCardCheck.java
+++ github.com/saragoodhue/CreditCardValidation.git/Credit Card Validation/src/ValidCardCheck.java
@@ -5,32 +5,47 @@ import java.io.IOException;
 public class ValidCardCheck
 	{
 		
-		static int numbers;
-
+		static int counter;
+		
 		public static void main(String[] args) throws IOException
 			{
 			
-				//test
-				int counter = 0;
 				
-				while(numbers.hasNext())
-					{
-						for(int i = 0; i < 99; i ++)
-							{
-								
-							}
-					}
+				fillArray();
+				
+
 			}
+		
+		public static void fillArray() throws IOException 
+		{
+			Scanner numbers = new Scanner(new File( "creditcardnumbers.txt" ));
+			
+			while(numbers.hasNext())
+				{
+					String cardNumber = numbers.nextLine();
+					System.out.println(cardNumber);
+					
+					while (cardNumber.length() > 0)
+						{
+							int n = 0;
+							
+							if(n % 2 == 0)
+								{
+									 int newValue = Integer.parseInt(cardNumber.substring(n, n + 1)) * 2;
+								}
+							
+							int numberPlace = Integer.parseInt(cardNumber.substring(n, n + 1));
+							int digitCheck = numberPlace % 10;
+							n += 1;
+							
+							
+						}
+				}
+		}
 		
 		public static void digitStrip() throws IOException
 		{
-			Scanner numbers = new Scanner(new File( "creditcardnumbers.txt" ));
-			int cardNumber = numbers.nextInt();
 
-			while (cardNumber > 0)
-				{
-					int digitCheck = cardNumber % 10;
-				}
 		}
 
 	}
