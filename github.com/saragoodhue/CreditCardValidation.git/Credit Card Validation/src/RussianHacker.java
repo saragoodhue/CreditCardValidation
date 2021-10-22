ddc692693d36c2a249df9dfaf18d1fa7cd79ce75
sara.goodhue <sara.goodhue@MU-C1-12.mullen.pvt.k12.co.us>
2021-10-22 11:05:20 -0600

    works

---
+++ github.com/saragoodhue/CreditCardValidation.git/Credit Card Validation/src/RussianHacker.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class RussianHacker
+	{
+
+		public static void main(String[] args) throws IOException
+			{
+				
+
+			}
+
+	}
--- github.com/saragoodhue/CreditCardValidation.git/Credit Card Validation/src/ValidCardCheck.java
+++ github.com/saragoodhue/CreditCardValidation.git/Credit Card Validation/src/ValidCardCheck.java
@@ -4,78 +4,57 @@ import java.io.IOException;
     
 public class ValidCardCheck
 	{
-		
-		static int counter;
-		static int sum = 0;
-		static int n = 0;
-		
+
 		public static void main(String[] args) throws IOException
 			{
-			
-				
 				fillArray();
-				//System.out.println(counter);
-
 			}
 		
 		public static void fillArray() throws IOException 
 		{
-			Scanner numbers = new Scanner(new File( "creditcardnumbers.txt" ));
+			Scanner numbers = new Scanner(new File( "realNumbers.txt" ));
 			int counter = 0; // counting valid card numbers
 			
 			while(numbers.hasNext())
 				{
 					String cardNumber = numbers.nextLine();
-					System.out.println(cardNumber);
-					sum = 0;
+					int[] nums = new int[cardNumber.length()];
 					
-					
-					
-					while (cardNumber.length() > 0 && cardNumber.length() < 17)
+					for (int i = 0; i < cardNumber.length(); i++)
 						{
-							for (int i = 0; i < cardNumber.length(); i++)
-								{
-									int numberPlace = Integer.parseInt(cardNumber.substring(i, i + 1));
-								}
-							 // digit stripping
-							int digitCheck = numberPlace % 10;		
-							
-							
-							if(n % 2 == 0) // doubling numbers in even places and checking for double digits
-								{
-									 int number = (Integer.parseInt(cardNumber.substring(n, n + 1))); // multiplying the first digit and every other one after by 2
-									 int doubleNumber = number * 2;
-									 if(doubleNumber > 10)
-										 {
-											 doubleNumber = Integer.parseInt(cardNumber.substring(0, 1)) + Integer.parseInt(cardNumber.substring(1, 2));
-										 }
-					
-									 sum += doubleNumber;
-									 n += 1;
-									 
-								}
-							else // adding the numbers in odd places to the sums
-								{
-									
-									sum += numberPlace;
-									n += 1;
-									
-								}
-							
-							//n += 1; // moving this a number place 
-							
-							
-							if(sum % 10 == 0) // checking if the card is valid
-								{
-									counter ++;
-								}
-							
-							
+							nums[i] = Integer.parseInt(cardNumber.substring(i, i + 1));
 						}
 					
-			
+					for (int i = nums.length - 2; i >= 0; i = i - 2)
+						{
+							int newNum = nums[i];
+							newNum = newNum * 2;
+							
+							if(newNum > 9)
+								{
+									newNum = newNum % 10 + 1;
+								}
+							nums[i] = newNum;
+						}
+							
+					int totalSum = 0;
 					
+					for(int i = 0; i < nums.length; i ++)
+						{
+							totalSum += nums[i];		
+						
+						}
+					if(totalSum % 10 == 0)
+						{
+							counter++;
+							System.out.println(cardNumber + " is a valid number");
+						}
+					else
+						{
+							System.out.println(cardNumber + " is not a valid number");
+						}	
 				}
+			System.out.println("There are " + counter + " valid credit card numbers");
 		}
 		
 
